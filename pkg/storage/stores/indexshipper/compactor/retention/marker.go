4cd0a4626d3fc1fd8630f31fa0313836a704dafd
Mathew <mat999@gmail.com>
2023-11-17 15:37:57 +1100

    slow down compaction as necessary

--- pkg/storage/stores/indexshipper/compactor/retention/marker.go
+++ pkg/storage/stores/indexshipper/compactor/retention/marker.go
@@ -10,6 +10,7 @@ import (
 	"path/filepath"
 	"sort"
 	"strconv"
+	strings "strings"
 	"sync"
 	"time"
 
@@ -297,6 +298,17 @@ func (r *markerProcessor) processPath(path string, deleteFunc func(ctx context.C
 			defer wg.Done()
 			for key := range queue {
 				if err := processKey(r.ctx, key, dbUpdate, deleteFunc); err != nil {
+					errstr := err.Error()
+
+					// 5s delay for throttling errors, or 50x errors.
+					if strings.HasPrefix(errstr, "SlowDown:") || strings.Contains(errstr, "status code: 50") {
+						time.Sleep(time.Second * 5)
+
+						if err := processKey(r.ctx, key, dbUpdate, deleteFunc); err == nil {
+							continue
+						}
+					}
+
 					level.Warn(util_log.Logger).Log("msg", "failed to delete key", "key", key.key.String(), "value", key.value.String(), "err", err)
 					time.Sleep(100 * time.Millisecond)
 				}
